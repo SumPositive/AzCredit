d1dbd0693c46b637b116203ad74942c498322c35
sanwax <m@sanwax.com>
2016-04-16 13:47:40 +0900

    Code2016 start, CocoaPods install

---
+++ Source/Pods/Target Support Files/Pods-AzCredit/Pods-AzCredit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AzCredit : NSObject
+@end
+@implementation PodsDummy_Pods_AzCredit
+@end
---
+++ Source/Pods/Target Support Files/Pods-AzCreditS1Pad/Pods-AzCreditS1Pad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AzCreditS1Pad : NSObject
+@end
+@implementation PodsDummy_Pods_AzCreditS1Pad
+@end
